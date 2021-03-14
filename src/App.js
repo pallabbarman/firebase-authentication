0a9bfb19bf7abdb5bae1eed92a9b764e46c6df8d
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-03-14 22:48:55 +0600

    update username

--- src/App.js
+++ src/App.js
@@ -18,11 +18,12 @@ function App() {
         photo: '',
     });
 
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    // const fbProvider = new firebase.auth.FacebookAuthProvider();
     const handleSignIn = () => {
         firebase
             .auth()
-            .signInWithPopup(provider)
+            .signInWithPopup(googleProvider)
             .then((res) => {
                 const { displayName, photoURL, email } = res.user;
                 const signInUser = {
@@ -38,6 +39,24 @@ function App() {
                 console.log(err.message);
             });
     };
+
+    // const handleFbSignIn = () => {
+    //     firebase
+    //         .auth()
+    //         .signInWithPopup(fbProvider)
+    //         .then((result) => {
+    //             const { credential } = result;
+    //             const { user } = result;
+    //             console.log(user);
+    //             const { accessToken } = credential;
+    //         })
+    //         .catch((error) => {
+    //             const errorCode = error.code;
+    //             const errorMessage = error.message;
+    //             const { email } = error;
+    //             const { credential } = error;
+    //         });
+    // };
 
     const handleSignOut = () => {
         firebase
@@ -59,6 +78,17 @@ function App() {
             });
     };
 
+    const updateUserName = (name) => {
+        const user = firebase.auth().currentUser;
+        user.updateProfile({
+            displayName: name,
+        })
+            .then(() => {})
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     const handleSubmit = (e) => {
         if (newUser && user.email && user.password) {
             firebase
@@ -69,6 +99,7 @@ function App() {
                     newUserInfo.error = '';
                     newUserInfo.success = true;
                     setUser(newUserInfo);
+                    updateUserName(user.name);
                 })
                 .catch((error) => {
                     const newUserInfo = { ...user };
@@ -81,11 +112,12 @@ function App() {
             firebase
                 .auth()
                 .signInWithEmailAndPassword(user.email, user.password)
-                .then(() => {
+                .then((res) => {
                     const newUserInfo = { ...user };
                     newUserInfo.error = '';
                     newUserInfo.success = true;
                     setUser(newUserInfo);
+                    console.log(res.user);
                 })
                 .catch((error) => {
                     const newUserInfo = { ...user };
@@ -124,6 +156,8 @@ function App() {
                     Sign In
                 </button>
             )}
+            <br />
+            {/* <button type="submit">Sign in using Facebook</button> */}
             {user.isSignIn && (
                 <div>
                     <p>Welcome, {user.name}</p>
@@ -159,7 +193,7 @@ function App() {
                     required
                 />
                 <br />
-                <input type="submit" value="Submit" />
+                <input type="submit" value={newUser ? 'Sign Up' : 'Sign In'} />
             </form>
             <p style={{ color: 'red' }}>{user.error}</p>
             {user.success && (
